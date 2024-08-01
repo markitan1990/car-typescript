eb5f2c4e4bf924c54bd6916e88f905b6949a129a
Маркитан Иван <markitan.i@olimp.bet>
2024-08-01 10:29:25 +0300

    upd

---
+++ src/components/contact-data/contact-data.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+export function ContactData() {
+    return (
+        <>
+            <div className="contact-data">
+                <div className='home-data'>
+                    <h1>Покупка и доставка  автомобилей</h1>
+                    <p>Поможем подобрать и купить авто из США, Европы,<br/> Южной Кореи и Китая на аукционе без<br/> посредников и переплат</p>
+                    <div className='home-button'>
+                     
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
