2849e4c068a6f9644e6d18e18c25232c94cd5ffd
klonirovan89 <klonirovan89@gmail.com>
2024-08-14 23:54:28 +0300

    changed styles home-page component

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,15 +1,15 @@
-import './home-page.css'
+import s from './home-page.module.scss'
 import { ButtonContactUs } from '../button/button'
 
 export function Home() {
     return (
         <>
-            <div className="home">
-                <div className='home-data'>
+            <div className={s.home}>
+                <div className={s.home_data}>
                     <h1>Покупка и доставка  автомобилей</h1>
                     <p>Поможем подобрать и купить авто из США, Европы,<br/> Южной Кореи и Китая на аукционе без<br/> посредников и переплат</p>
-                    <div className='home-button'>
-                        <ButtonContactUs value="Купить сейчас" className='btn-1'/>
+                    <div className={s.home_button}>
+                        <ButtonContactUs value="Купить сейчас" className={s.btn_1}/>
                         <ButtonContactUs value="Подробнее" />
                     </div>
                 </div>
