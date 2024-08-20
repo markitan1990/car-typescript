15ef609f851ef1506ebd1400894817e2c6c1ff15
klonirovan89 <klonirovan89@gmail.com>
2024-08-20 10:18:45 +0300

    fixed style AboutUs component

--- src/layout/section/aboutUs/AboutUs.tsx
+++ src/layout/section/aboutUs/AboutUs.tsx
@@ -12,23 +12,28 @@ export const AboutUs = () => {
                 <Image className={s.about_us_img2} src="/img/image.png" alt="Главная"/>
             </div>
             <div className={s.text_info}>
-                    <div className={s.blue_text}>
-                        <p>Добро пожаловать в DRIVE HUB</p>
-                        <div></div>
-                    </div>
-                    <h3>Мы Известны Своим Сервисом</h3>
-                    <div className={s.block_about}>
-                        <h4>Drive Hub - это самая привлекательная, креативная, современная компания премиум-класса
-                            автодилеров. </h4>
-                        <ul>
-                            {aboutUsText.map(i =>
-                                <li key={i.id}>
-                                    <span><Icon iconId={'arrow'} height={'12'} width={'16'} viewBox={'0 0 16 12'}/></span>
-                                    {i.value}
-                                </li>
-                            )}
-                        </ul>
-                    </div>
+                <div className={s.blue_text}>
+                    <p>Добро пожаловать в DRIVE HUB</p>
+                    <div></div>
+                </div>
+                <h3>Мы Известны Своим Сервисом</h3>
+                <div className={s.block_about}>
+                    <h4>Drive Hub - это самая привлекательная, креативная, современная компания премиум-класса
+                        автодилеров. </h4>
+                    <ul>
+                        {aboutUsText.map(i =>
+                            <li key={i.id}>
+                                <span><Icon iconId={'arrow'} height={'12'} width={'16'} viewBox={'0 0 16 12'}/></span>
+                                {i.value}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <a href="http://localhost:3000/" className={s.link}>
+                    <span>Хочешь больше узнать об этом?</span>
+                    <span>Жми</span>
+                    <Icon iconId={'arrow_up'} height={'16'} width={'16'} viewBox={'0 0 16 16'}/>
+                </a>
             </div>
         </div>
     );
